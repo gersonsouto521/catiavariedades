65f1ebe51e3e5cc15d607ee9b5dce658587a3788
gersonsouto521 <46946962+gersonsouto521@users.noreply.github.com>
2020-10-17 11:10:09 -0300

    add2 run.py

--- run.py
+++ run.py
@@ -1,3 +1,4 @@
+import os
 from environs import Env
 env = Env()
 env.read_env()
@@ -8,4 +9,5 @@ app = create_app()
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5001, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
